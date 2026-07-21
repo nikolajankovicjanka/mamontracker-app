d8b26d0a4f7de7967fc975e07e3166caa0af88f8
janka <nikolajankovicjanka@gmail.com>
2026-07-21 17:10:51 +0200

    fix: axios.ts

--- apps/web/src/lib/axios.ts
+++ apps/web/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ?? 'http://mamont.localhost:8080',
+    baseURL: import.meta.env.VITE_API_URL || window.location.origin,
     withCredentials: true,
     withXSRFToken: true,
     headers: {
