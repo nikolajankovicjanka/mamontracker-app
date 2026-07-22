2dea9f87ec77fda21f266446d26580ce64c5c713
janka <nikolajankovicjanka@gmail.com>
2026-07-22 12:15:43 +0200

    feat: middleware za season cookies odvojeno za admin i app login. update

--- apps/api/app/Http/Middleware/ConfigureSessionCookie.php
+++ apps/api/app/Http/Middleware/ConfigureSessionCookie.php
@@ -8,26 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ConfigureSessionCookie
 {
-    /**
-     * Handle an incoming request.
-     */
     public function handle(Request $request, Closure $next): Response
     {
         $adminHost = config('app.admin_host');
 
-        if ($request->getHost() === $adminHost) {
-            config([
-                'session.cookie' => 'mamontrack_admin_session',
-            ]);
-        } else {
-            config([
-                'session.cookie' => 'mamontrack_tenant_session',
-            ]);
-        }
-
-        logger()->info('Session cookie', [
-            'host' => $request->getHost(),
-            'cookie' => config('session.cookie'),
+        config([
+            'session.cookie' => $request->getHost() === $adminHost
+                ? 'mamontrack_admin_session'
+                : 'mamontrack_tenant_session',
         ]);
 
         return $next($request);
